d9c0bd170c36df2ee193d99e01f7a9c6c616b7f7
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-02-08 17:02:03 +0300

    feat: webpack setup

---
+++ src/scripts/index.js
@@ -0,0 +1,4 @@
+import '../index.css';
+import splidePhotos from "./slider";
+
+splidePhotos.mount();
---
+++ src/scripts/slider.js
@@ -0,0 +1,18 @@
+import Splide from '@splidejs/splide';
+
+const splidePhotos = new Splide( '#splide_place_photos', {
+  classes: {
+    pagination  : 'splide__pagination',
+		page        : 'splide__pagination__page product__photos-pagination-buttons'
+  },
+  perPage: 3,
+  focus: 0,
+  autoWidth: true,
+  omitEnd: true,
+  drag: 'free',
+  snap: false,
+  arrows: false,
+  pagination: true,
+} );
+
+export default splidePhotos;
