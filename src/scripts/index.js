e9f47a4f64b8d6904de3d524f4a226c8755321c6
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-02-08 20:56:25 +0300

    feat: goods quantity script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,25 @@ import '@splidejs/splide/css';
 import splidePhotos from "./slider";
 
 splidePhotos.mount();
+
+const minusButton    = document.querySelector('.order__quantity-button_minus');
+const plusButton     = document.querySelector('.order__quantity-button_plus');
+const quantityResult = document.querySelector('.order__quantity-result');
+
+let goodsQuantity = 10;
+
+function addGoods() {
+  goodsQuantity += 1;
+  quantityResult.textContent = `${goodsQuantity} шт.`;
+}
+
+function reduceGoods() {
+  if (goodsQuantity > 0) {
+    goodsQuantity -= 1;
+    quantityResult.textContent = `${goodsQuantity} шт.`;
+  }
+}
+
+minusButton.addEventListener('mousedown', reduceGoods);
+plusButton.addEventListener('mousedown', addGoods);
+
